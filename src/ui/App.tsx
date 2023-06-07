ec22e25ec96b05f35b469cd1393c75b5da0232f5
Evseev Anton <soki1997@mail.ru>
2023-06-07 13:29:05 +0500

    привет

---
+++ src/ui/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Menu from './pages/menu/menu';
+import Header from './pages/header/header';
+import Content from './pages/content/content';
+
+function App() {
+    return (
+        <div className="app-wrapper ">
+            <Menu/>
+            <Header/>
+            <div className="app-wrapper-content">
+                <Content/>
+
+            </div>
+        </div>
+    );
+}
+
+export default App;
